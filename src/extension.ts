93a26dbe5317f91ed4be7b164c75113a7df030be
Yusuke Sasaki <y_sasaki@nuem.nagoya-u.ac.jp>
2016-07-12 05:05:49 +0900

    simplify source

--- src/extension.ts
+++ src/extension.ts
@@ -3,28 +3,28 @@
 import {commands, ExtensionContext, window} from 'vscode';
 import * as cp from 'child_process';
 
-function sayHello() {
-    let editor = window.activeTextEditor;
-    // get selected text.
-    let text = editor.document.getText(editor.selection);
-
-    var pandoc = cp.exec("pandoc --from=markdown --to=latex", (err, stdout, stderr) => {
-        if (err) {
-            console.error("pandoc failed: %s", stderr);
-            return;
-        }
-
-        window.activeTextEditor.edit((edit) => {
-            edit.replace(editor.selection, stdout);
-        });
-    });
-
-    // send text.
-    pandoc.stdin.write(text);
-    pandoc.stdin.end();
-}
-
 export function activate(context: ExtensionContext) {
-    let disposable = commands.registerCommand('extension.sayHello', sayHello); 
+    let editor = window.activeTextEditor;
+
+    let disposable = commands.registerCommand('extension.convertPandoc', () => {
+        // The text to be converted.
+        let text = editor.document.getText(editor.selection);
+
+        // create the instance of pandoc executable with callback.
+        var pandoc = cp.exec("pandoc --from=markdown --to=latex", (err, stdout, stderr) => {
+            if (err) {
+                console.error("pandoc failed: %s", stderr);
+                return;
+            }
+
+            window.activeTextEditor.edit((edit) => {
+                edit.replace(editor.selection, stdout);
+            });
+        });
+
+        // send text from stdin.
+        pandoc.stdin.write(text);
+        pandoc.stdin.end();
+    }); 
     context.subscriptions.push(disposable);
 }
